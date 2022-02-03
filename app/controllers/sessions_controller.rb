f2fbb2e9a36dea789310239894333ece0d87fbd1
theodoum <theodoum1@gmail.com>
2022-02-03 23:14:39 +0100

    Push save

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: user_params[:email])
 
     if @user && @user.password == user_params[:password]
-      cookies[:user_id] = @user.id
+      session[:user_id] = @user.id
       redirect_to home_path
     else
       flash.now[:notice] = "Mot de passe ou email invalide"
